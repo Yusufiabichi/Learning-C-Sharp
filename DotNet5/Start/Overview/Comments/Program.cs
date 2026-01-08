92c7d540478db742e370717d74a6590b551d9c44
Yusufiabichi <yusufiabichi@yahoo.com>
2026-01-08 16:24:20 +0100

    Explicit conversion

--- DotNet5/Start/Overview/Comments/Program.cs
+++ DotNet5/Start/Overview/Comments/Program.cs
@@ -6,10 +6,19 @@ namespace Comments
     {
         /// XML Comments are used to help provide documentation
         /// They start with triple-slashes and have a special syntax
+        /// <summary>
+        /// This is the main sample application function
+        /// </summary>
+        /// <param name='args'>An array of string arguments from the command line </param>
+        /// <returns>
+        /// No return value
+        /// </returns>
         static void Main(string[] args)
         {
-
+            // Single line comments
             Console.WriteLine("Hello World!");
+            /* Multi linr comments
+            you can have comments in many lines */
 
         }
     }
