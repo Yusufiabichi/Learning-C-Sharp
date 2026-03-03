aa512ac899f9fe4c7d16841291f2b12195dd55fd
Yusufiabichi <yusufiabichi@yahoo.com>
2026-03-03 17:38:11 +0100

    c# Tuples

--- DotNet5/Start/Functions/RefAndOutParams/obj/Debug/net5.0/RefAndOut.AssemblyInfo.cs
+++ DotNet5/Start/Functions/RefAndOutParams/obj/Debug/net5.0/RefAndOut.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RefAndOut")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+090ad7fb27968e056204a9d0282d1febd77555e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38ea750bf6b081cd557e2c8ab3e71c98d9c8fa5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("RefAndOut")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RefAndOut")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
